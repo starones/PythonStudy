2f04759a01b03a1a1fd36c294b0f01ddb8555695
starones <itstarone172@gmail.com>
2022-03-18 15:57:35 +0900

    lesson02

---
+++ lesson02.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# ------変数について-------
+name = "スーパーマン"
+print(name)
+
+num = 7
+print(num)
+
+apple_price = 200
+apple_count = 8
+
+
+# -------変数の計算--------
+# apple_price と apple_count を掛けた結果を、変数 total_price に代入
+total_price = apple_price * apple_count
+
+# total_price の値を出力
+print(total_price)
+
+
+# -------変数の上書き--------
+money = 2000
+print(money)
+
+# 変数 money に 5000 を足して、変数 money を上書きしてください
+money += 5000
+
+# 変数 money の値を出力してください
+print(money)
